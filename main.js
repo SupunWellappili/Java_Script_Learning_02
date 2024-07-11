a9616ce02542385fd438faaca4b36f9c38ddd375
SupunWellappili <supunchandana1814@gmail.com>
2024-07-11 13:42:08 +0530

    alert,confirm,defalut,page link

---
+++ main.js
@@ -0,0 +1,8 @@
+console.log("hi main.js");
+const sName = confirm("Are You Amila Silva? ");
+if (sName == true) {
+    let yName = prompt('Enter Your Age?', '50');//Add to Value
+    console.log("Name is : " + yName);
+} else {
+    alert("Wrong Details Added!");
+}
