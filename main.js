c1042c47d56729e9f2b7022e627e0af7a372b2e0
SupunWellappili <supunchandana1814@gmail.com>
2024-07-11 14:36:44 +0530

    Js variable init,console.log , console.table

--- main.js
+++ main.js
@@ -6,3 +6,47 @@ if (sName == true) {
 } else {
     alert("Wrong Details Added!");
 }
+
+console.error("This is an error");
+
+
+let a ={
+    name: 'Supun',
+    city : 'Galle',
+    salary : 32000,
+}
+
+let b ={
+    name: 'Supun',
+    city : 'Galle',
+    salary : 32000,
+}
+
+let c ={
+    name: 'Chandana',
+    city : 'Matara',
+    salary : 32000,
+}
+
+let d ={
+    name: 'Wellappili',
+    city : 'Hambantota',
+    salary : 32000,
+}
+
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
+
+console.log("---------------")
+
+console.log({a,b,c,d});
+
+console.log("---------------")
+
+console.log(a,b,c,d);
+
+console.log("---------------")
+
+console.table({a,b,c,d});
